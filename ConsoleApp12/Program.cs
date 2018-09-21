a0da6f806449c1027c4726374d2612173fec0db4
parasbajania <43333769+parasbajania@users.noreply.github.com>
2018-09-21 10:42:05 +0530

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb1 = 10;
+            int numb2;
+            numb2 = numb1++;
+            Result=
+            Console.WriteLine(numb1);
+            Console.WriteLine(numb2);
+        }
+    }
+}
